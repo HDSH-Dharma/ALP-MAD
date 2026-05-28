70cdd4b858ba9623bbb38e056e022fd98a703829
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2026-05-28 10:43:51 +0700

    Initial Commit

---
+++ ALP_MAD/ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MADApp.swift
+//  ALP_MAD
+//
+//  Created by Dharma on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALP_MADUITests/ALP_MADUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  ALP_MADUITestsLaunchTests.swift
+//  ALP_MADUITests
+//
+//  Created by Dharma on 28/05/26.
+//
+
+import XCTest
+
+final class ALP_MADUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
