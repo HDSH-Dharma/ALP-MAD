55a8268d0a8fbf05c226c50d15e47cc88881028d
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2026-05-28 14:40:49 +0700

    feature 2: solve-merge-conflict

--- ALP_MAD/Model/Trip.swift
+++ ALP_MAD/Model/Trip.swift
@@ -16,6 +16,7 @@ final class Trip {
     var endDate: Date
     var currency: String
  
+    @Relationship(deleteRule: .cascade) var destinations: [Destination] = []
     @Relationship(deleteRule: .cascade, inverse: \BudgetItem.trip)
     var budgetItems: [BudgetItem]
  
