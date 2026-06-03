75ec0496b958f7b9f5ee85f75f2305c1a64e9585
jefereysantosa <jefereyteddy88888@gmail.com>
2026-06-03 12:25:23 +0700

    adding primary color palette

---
+++ ALP_MAD/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ALP_MAD
+//
+//  Created by student on 03/06/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let roseGold = Color(red: 0.89, green: 0.61, blue: 0.63)
+    static let roseGoldDark = Color(red: 0.72, green: 0.43, blue: 0.47)
+    static let roseGoldLight = Color(red: 0.95, green: 0.85, blue: 0.86)
+}
